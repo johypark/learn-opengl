30738bff37f03b77b8c248ab8e8d6bb828eb4c7b
Joohyung Park <johy@dabyeol.com>
2021-04-12 23:09:42 +0900

    Test

---
+++ 04/4-7.cc
@@ -0,0 +1,28 @@
+// Copyright 2021 Johy. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for license information.
+
+#include <gl/glut.h>
+
+void MyDisplay() {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glBegin(GL_POLYGON);
+
+  glVertex3f(-0.5, -0.5, 0.0);
+  glVertex3f(0.5, -0.5, 0.0);
+  glVertex3f(0.5, 0.5, 0.0);
+  glVertex3f(-0.5, 0.5, 0.0);
+
+  glEnd();
+  glFlush();
+}
+
+int main(int argc, char* argv[]) {
+  glutInit(&argc, argv);
+  glutCreateWindow("OpenGL Drawing Example");
+
+  glutDisplayFunc(MyDisplay);
+  glutMainLoop();
+
+  return 0;
+}
