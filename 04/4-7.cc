fbc1386dda8b0ed6b515a87582fa5f3cc3de2656
Joohyung Park <johy@dabyeol.com>
2021-04-13 05:29:31 +0900

    Support macOS

--- 04/4-7.cc
+++ 04/4-7.cc
@@ -2,27 +2,27 @@
 // Licensed under the MIT License.
 // See LICENSE file in the project root for license information.
 
-#include <gl/glut.h>
+#define GL_SILENCE_DEPRECATION
+
+#include <GL/glut.h>
 
 void MyDisplay() {
   glClear(GL_COLOR_BUFFER_BIT);
-  glBegin(GL_POLYGON);
 
+  glBegin(GL_POLYGON);
   glVertex3f(-0.5, -0.5, 0.0);
   glVertex3f(0.5, -0.5, 0.0);
   glVertex3f(0.5, 0.5, 0.0);
   glVertex3f(-0.5, 0.5, 0.0);
-
   glEnd();
+
   glFlush();
 }
 
 int main(int argc, char* argv[]) {
   glutInit(&argc, argv);
   glutCreateWindow("OpenGL Drawing Example");
-
   glutDisplayFunc(MyDisplay);
-  glutMainLoop();
 
-  return 0;
+  glutMainLoop();
 }
